d5a6b956296a2d28ebab6325e200f0276826caaa
galind <guillem.yt1@gmail.com>
2023-06-18 22:00:17 +0200

    Added command to view settings and view to update it

--- cogs/logs.py
+++ cogs/logs.py
@@ -1,19 +1,75 @@
-from discord import Interaction, TextChannel, app_commands
+from importlib import reload
+
+from discord import ButtonStyle, Embed, Interaction, TextChannel, app_commands
 from discord.ext import commands
-from cogs.utils import mongo
+from discord.ui import Button, View
 
 from bot import Bot
+from cogs.utils import mongo
+
+logs_options = [
+    'Joined',
+    'Left',
+    'Edited',
+    'Deleted'
+]
+
+
+def logs_embed(options: list[str], settings: dict[str, str]) -> Embed:
+    title = 'Logs Settings'
+    footer =\
+        'Click any of the buttons below to reset the selected channel'
+    embed = Embed(title=title)
+    embed.set_footer(text=footer)
+
+    for option in options:
+        value = settings.get(option.lower(), None)
+        if value:
+            value = f'<#{value}>'
+        else:
+            value = 'Not specified'
+        embed.add_field(name=option, value=value)
+
+    return embed
+
+
+class ResetLogsButton(Button):
+    def __init__(self, option: str, has_value: bool) -> None:
+        style = ButtonStyle.danger
+        disabled = not has_value
+        super().__init__(style=style, label=option, disabled=disabled)
+
+    async def callback(self, interaction: Interaction) -> None:
+        guild = mongo.Guild(interaction.guild_id)
+        query = {'$set': {self.label.lower(): None}}
+        await guild.update(query)
+        guild_settings = await guild.check()
+
+        embed = logs_embed(logs_options, guild_settings)
+        view = ResetLogsView(logs_options, guild_settings)
+        await interaction.response.edit_message(embed=embed, view=view)
+
+
+class ResetLogsView(View):
+    def __init__(self, options: list[str], settings: dict[str, str]) -> None:
+        super().__init__(timeout=None)
+        for option in options:
+            has_value = bool(settings.get(option.lower(), None))
+            self.add_item(ResetLogsButton(option, has_value))
 
 
 class Logs(commands.GroupCog, group_name='logs'):
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
+    async def cog_load(self) -> None:
+        reload(mongo)
+
     async def update_guild(
-        self, guild_id: int, channel_id: int, action: str
+        self, guild_id: int, channel_id: int, option: str
     ) -> None:
         guild = mongo.Guild(guild_id)
-        query = {'$set': {action: channel_id}}
+        query = {'$set': {option: channel_id}}
         await guild.update(query)
 
     async def interaction_response(self, interaction: Interaction) -> None:
@@ -51,6 +107,18 @@ class Logs(commands.GroupCog, group_name='logs'):
         """Get notified every time a user deletes a message"""
         await self.update_guild(interaction.guild_id, channel.id, 'deleted')
         await self.interaction_response(interaction)
+
+    @app_commands.command()
+    async def settings(self, interaction: Interaction) -> None:
+        """Check and update logs settings"""
+        guild = mongo.Guild(interaction.guild_id)
+        guild_settings = await guild.check()
+
+        embed = logs_embed(logs_options, guild_settings)
+        view = ResetLogsView(logs_options, guild_settings)
+        await interaction.response.send_message(
+            embed=embed, view=view, ephemeral=True
+        )
 
 
 async def setup(bot: Bot) -> None:
